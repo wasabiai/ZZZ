e6d880728e6920480c4874602a2b262ca19c378f
wasabiai <wasabisasibi71@hotmail.com>
2025-12-13 02:28:27 +0800

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ padding: 16, borderBottom: "1px solid #e5e7eb" }}>
+      <div style={{ fontSize: 18, fontWeight: 700 }}>Header</div>
+    </header>
+  );
+}
